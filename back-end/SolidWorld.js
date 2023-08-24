76e2c05311aa644d9dfdf8e087dd1d33853471f2
Louise <louise.borreani@gmail.com>
2023-08-24 13:19:37 +0200

    SolidWorld.js works

--- back-end/SolidWorld.js
+++ back-end/SolidWorld.js
@@ -3,43 +3,41 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 const path = require('path');
+const cheerio = require("cheerio");
+
 const port = process.env.PORT || 3000;
+const mongoURL = 'mongodb+srv://louise:Z04niNeVKEFR4erM@cluster0.miypx8l.mongodb.net/?authSource=Cluster0&authMechanism=SCRAM-SHA-1';
+const dbName = 'ReFi-Asset-Map';
+const collectionName = 'SolidWorld1';
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'C:/Users/louis/Documents/Work/Coding Practice/ReFi Asset Map/public/index.html'));
 });
 
-const mongoURL = 'mongodb+srv://louise:Z04niNeVKEFR4erM@cluster0.miypx8l.mongodb.net/?authSource=Cluster0&authMechanism=SCRAM-SHA-1';
-const dbName = 'ReFi-Asset-Map';
-
-const client = new MongoClient(mongoURL, {useUnifiedTopology: true});
-
-async function insertData(data) {
+app.get('/api/projects', async (req, res) => {
     try {
+        console.log('Before connecting to MongoDB');
+        const client = new MongoClient(mongoURL, { useUnifiedTopology: true });
         await client.connect();
-        console.log('Connected to MongoDB');
-
+        console.log('Connected to MongoDB'); // Log the connection status
         const db = client.db(dbName);
-        const collection = db.collection('SolidWorld1');
-
-        const result = await collection.insertMany(data);
-        console.log(`${result.insertedCount} documents inserted`, result.insertedIds);
+        const collection = db.collection(collectionName);
+        const projects = await collection.find().toArray();
+        res.json(projects);
     } catch (error) {
-        console.error('Error inserting data:', error);
+        console.error('Error fetching project data:', error);
+        res.status(500).json({ error: 'An error occurred while fetching project data.' });
     } finally {
         client.close();
-        console.log('Disconnected from MongoDB');
+        console.log('Disconnected from MongoDB'); // Log the disconnection status
     }
-};
-
-const cheerio = require ("cheerio");
+});
 
 const url = "https://app.solid.world/projects";
-
-const collectionName = 'SolidWorld1';
-
+console.log('Before axios GET request');
 axios.get(url)
     .then(async response => {
+        console.log('Axios GET request succesful');
         const html = response.data;
         const $ = cheerio.load(html);
 
@@ -48,15 +46,18 @@ axios.get(url)
 
         const nominatimUrl = "https://nominatim.openstreetmap.org/search";
 
+        const client = new MongoClient(mongoURL, { useUnifiedTopology: true });
         await client.connect();
+        console.log('Connected to MongoDB');
         const db = client.db(dbName);
-        const collection = db.collection(SolidWorld1);
+        const collection = db.collection(collectionName);
 
         for (const div of targetDivs) {
+            console.log('Processing a div');
             const location = $(div).text().trim();
 
             // Check if the project already exists in the database
-            const existingProject = await collection.findOne({location});
+            const existingProject = await collection.findOne({ location });
 
             if (!existingProject) {
                 // Send a GET request to Nominatim API
@@ -64,48 +65,44 @@ axios.get(url)
                     params: {
                         q: location,
                         format: 'json',
-                },
-            });
+                    },
+                });
 
-            if (nominatimResponse.data.length > 0) {
-                const coordinates = {
-                    lat: nominatimResponse.data[0].lat,
-                    lng: nominatimResponse.data[0].lon,
-                };
+                if (nominatimResponse.data.length > 0) {
+                    const coordinates = {
+                        lat: nominatimResponse.data[0].lat,
+                        lng: nominatimResponse.data[0].lon,
+                    };
 
-                extractedData.push({location, coordinates});
+                    extractedData.push({ location, coordinates });
+                }
             }
         }
-    }
 
-    if (extractedData.length > 0) {
-        insertData(extractedData);
-    } else {
-        console.log('No new data to insert.');
-    }
+        if (extractedData.length > 0) {
+            console.log('Inserting new data into the collection');
+            await insertData(extractedData, collection);
+        } else {
+            console.log('No new data to insert.');
+        }
 
-    // Close the connection after processing
-    client.close();
-})
+        // Close the connection after processing
+        client.close();
+    })
     .catch(error => {
         console.error('Error fetching the page:', error);
     });
 
-    app.get('/api/projects', async (req, res) => {
-        try {
-            await client.connect();
-            const db = client.db(dbName);
-            const collection = db.collection('SolidWorld1');
-            const projects = await collection.find().toArray();
-            res.json(projects);
-        } catch (error) {
-            console.error('Error fetching project data:', error);
-            res.status(500).json({error: 'An error occured while fetching project data.'});
-        } finally {
-            client.close();
-        }
-    });
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
+async function insertData(data, collection) {
+    try {
+        const result = await collection.insertMany(data);
+        console.log(`${result.insertedCount} documents inserted`, result.insertedIds);
+    } catch (error) {
+        console.error('Error inserting data:', error);
+    }
+}
+
--- back-end/server.js
+++ back-end/server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-const mongoURL = 'mongodb+srv://louise:Z04niNeVKEFR4erM@cluster0.miypx8l.mongodb.net/?authSource=Cluster0&authMechanism=SCRAM-SHA-1';
+const mongoURL = 'mongodb+srv://louise:Z04niNeVKEFR4erM@cluster0.miypx8l.mongodb.net/?retryWrites=true&w=majority';
 const dbName = 'ReFi-Asset-Map';
 
 const client = new MongoClient(mongoURL, { useUnifiedTopology: true });
@@ -43,29 +43,40 @@ axios.get(url)
         const html = response.data;
         const $ = cheerio.load(html);
 
-        const targetDivs = $('div.ProjectCard_ellipsis__1NASF');
+        const targetDivs = $('div.ProjectCard_card__3Yi_y');
         const extractedData = [];
 
         const nominatimUrl = "https://nominatim.openstreetmap.org/search";
-
         await client.connect();
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
 
-        for (const div of targetDivs) {
-            const location = $(div).text().trim();
+        for (const card of targetDivs) {
+            const location = $(card).find('div.ProjectCard_ellipsis__1NASF').text().trim();
+            console.log('Extracted location:', location);
+            const projectTitle = $(card).find('h2.ProjectCard_title__nsbeD').text().trim();
+            const assetLink = $(card).find('a.ProjectCard_link__KD__k').attr('href');
+
+            console.log('Extracted Data:', {
+                location,
+                projectTitle,
+                assetLink,
+            });
 
             // Check if the project already exists in the database
             const existingProject = await collection.findOne({ location });
+            console.log('Existing Project:', existingProject);
 
             if (!existingProject) {
                 // Send a GET request to Nominatim API
+                console.log('Nominatim API Request:', nominatimUrl + '?q=' + location);
                 const nominatimResponse = await axios.get(nominatimUrl, {
                     params: {
                         q: location,
                         format: 'json',
                     },
                 });
+                console.log('Nominatim API Response:', nominatimResponse.data);
 
                 if (nominatimResponse.data.length > 0) {
                     const coordinates = {
@@ -73,7 +84,13 @@ axios.get(url)
                         lng: nominatimResponse.data[0].lon,
                     };
 
-                    extractedData.push({ location, coordinates });
+                    extractedData.push({
+                        location,
+                        projectTitle,
+                        assetLink,
+                        coordinates,
+                        assetType: 'Forward Credit',
+                    });
                 }
             }
         }
