f38e4a23a3ebe42837bf2400d9a4a4d3c487ac7d
Louise <louise.borreani@gmail.com>
2023-08-30 18:47:53 +0200

    Modified map.js

--- public/js/map.js
+++ public/js/map.js
@@ -9,26 +9,33 @@ document.addEventListener('DOMContentLoaded', () => {
         center: [20, 1.5],
         zoom: 0,
     });
-});
 
     // Make a request to the back-end API to fetch project data
     axios.get('/api/projects')
         .then(response => {
+            console.log('Received project data:', response.data);
+            
             const projects = response.data;
 
             projects.forEach(project => {
-                const {location, coordinates} = project;
+                const {location, coordinates, projectTitle, description, assetType, assetLink} = project;
 
-                createMarkerAndPopup(
-                    [parseFloat(coordinates.lng), parseFloat(coordinates.lat)],
-                    location,
-                    projectTitle,
-                    assetType,
-                    assetLink,
-                );
+                // Create a marker for each project
+                const marker = new mapboxgl.Marker()
+                    .setLngLat([parseFloat(coordinates.lng), parseFloat(coordinates.lat)])
+                    .addTo(map);
+                
+                // Create a popup for each marker
+                const popup = new mapboxgl.Popup({closeButton: false, offset: 25})
+                    .setHTML(`
+                        <h3>${projectTitle}</h3>
+                        <p><a href="${assetLink}" target="_blank">Learn more</a></p>
+                    `);
+                
+                marker.setPopup(popup);
             });
-            console.log(response.data);
         })
         .catch(error => {
             console.error('Error fetching project data:', error);
         });
+});
