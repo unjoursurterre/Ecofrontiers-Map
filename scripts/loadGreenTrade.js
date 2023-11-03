96c9fc954b4ae2e73880cda2f38e1af8dcc1fe31
Louise <louise.borreani@gmail.com>
2023-11-03 15:31:46 +0100

    GreenTrade loading works

--- scripts/loadGreenTrade.js
+++ scripts/loadGreenTrade.js
@@ -1,105 +1,120 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 const { MongoClient } = require('mongodb');
+const axios = require('axios');
 
 const mongoURL =
   'mongodb+srv://louise:Z04niNeVKEFR4erM@cluster0.miypx8l.mongodb.net/?authSource=Cluster0&authMechanism=SCRAM-SHA-1';
 const dbName = 'ReFi-Asset-Map';
 const collectionName = 'GreenTrade';
 
+const nominatimUrl = 'https://nominatim.openstreetmap.org/search';
+
 const loadGreenTradeData = async (db) => {
   try {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
     const url = 'https://app.greentrade.tech/project/index';
-    const response = await axios.get(url);
-    console.log('Axios GET request successful');
-    const html = response.data;
-    console.log(response.data);
-    const $ = cheerio.load(html);
+    await page.goto(url);
 
-    const targetDivs = $('.project-list-items');
-    console.log(targetDivs.length);
+    // Wait for the project cards to load (you may need to adjust the selector and waiting time)
+    await page.waitForSelector('.project-card-body', { timeout: 5000 });
+
+    const targetDivs = await page.$$eval('.project-card-body', (divs) => {
+      return divs.map((div) => {
+        const locationElement = div.querySelector('table tbody tr:nth-child(1) td:nth-child(2)');
+        const location = locationElement.textContent.trim();
+
+        const projectTitleElement = div.querySelector('.project-card-body__name');
+        const projectTitle = projectTitleElement.textContent.trim();
+
+        const descriptionElement = div.querySelector('table tbody tr:nth-child(2) td:nth-child(2)');
+        const description = descriptionElement.textContent.trim();
+
+        const assetLinkElement = div.querySelector('.left a');
+        const assetLink = assetLinkElement.getAttribute('href');
+
+        return {
+          location,
+          projectTitle,
+          description,
+          assetLink,
+        };
+      });
+    });
+
+    async function insertData(collection, data) {
+      try {
+        const result = await collection.insertMany(data);
+        console.log(`${result.insertedCount} documents inserted`, result.insertedIds);
+      } catch (error) {
+        console.error('Error inserting data:', error);
+      }
+    };    
+
+    await browser.close();
+
     const extractedData = [];
-
-    const nominatimUrl = 'https://nominatim.openstreetmap.org/search';
 
     const collection = db.collection(collectionName);
 
     console.log('Before for loop');
-    for (const card of targetDivs) {
-      console.log('Processing a div');
-      
-      const table = $('.project-card-body__provider table');
+    for (const data of targetDivs) {
+      console.log('Processing data:', data);
 
-      const locationRow = $('tr', table)[0];
-      const descriptionRow = $('tr', table)[1];
-      
-      const location = $(locationRow).find('td').eq(1).text().trim();
-      const description = $(descriptionRow).find('td').eq(1).text().trim();
+      const { location, projectTitle, description, assetLink } = data;
 
-      const projectTitleElement = $(card).find('.project-card-body__name');
-      const projectTitle = projectTitleElement.text().trim();
-                   
-      const assetLinkElement = $(card).find('.left');
-      const assetLink = assetLinkElement.attr('href');
-        
-      console.log('Extracted Data:', {
+      // Check if the project already exists in the database
+      const filter = { projectTitle };
+      const existingProject = await collection.findOne(filter);
+      console.log('Existing Project:', existingProject);
+
+        // Send a GET request to Nominatim API
+    console.log('Nominatim API Request:', nominatimUrl + '?q=' + location);
+    const nominatimResponse = await axios.get(nominatimUrl, {
+      params: {
+        q: location,
+        format: 'json',
+      },
+    });
+    console.log('Nominatim API Response:', nominatimResponse.data);
+
+    if (nominatimResponse.data.length > 0) {
+      const coordinates = {
+        lat: nominatimResponse.data[0].lat,
+        lng: nominatimResponse.data[0].lon,
+      };
+
+      const newData = {
         location,
         projectTitle,
         assetLink,
         description,
-      });
-        
-      // Check if the project already exists in the database
-      const existingProject = await collection.findOne({ location });
-      console.log('Existing Project:', existingProject);
-        
-      if (!existingProject) {
-        // Send a GET request to Nominatim API
-        console.log('Nominatim API Request:', nominatimUrl + '?q=' + location);
-        const nominatimResponse = await axios.get(nominatimUrl, {
-          params: {
-            q: location,
-            format: 'json',
-          },
-        });
-        console.log('Nominatim API Response:', nominatimResponse.data);
+        coordinates,
+        assetType: 'Forward Carbon Offset',
+        issuer: 'GreenTrade'
+      };
 
-        if (nominatimResponse.data.length > 0) {
-          const coordinates = {
-            lat: nominatimResponse.data[0].lat,
-            lng: nominatimResponse.data[0].lon,
-          };
-
-          extractedData.push({
-            location,
-            projectTitle,
-            assetLink,
-            description,
-            coordinates,
-            assetType: 'Forward Carbon Offset',
-          });
-        }
+      if (existingProject) {
+        // If the project exists, update the existing document
+        await collection.updateOne(filter, { $set: newData });
+        console.log('Updated data for', projectTitle);
+      } else {
+        // If the project doesn't exist, insert a new document
+        extractedData.push(newData);
+        console.log('Inserted data for', projectTitle);
       }
     }
-
-    if (extractedData.length > 0) {
-      await insertData(collection, extractedData);
-    } else {
-      console.log('No new data to insert.');
-    }
-  } catch (error) {
-    console.error('Error scraping data:', error);
-    throw error;
   }
-};
 
-async function insertData(collection, data) {
-  try {
-    const result = await collection.insertMany(data);
-    console.log(`${result.insertedCount} documents inserted`, result.insertedIds);
-  } catch (error) {
-    console.error('Error inserting data:', error);
+  if (extractedData.length > 0) {
+    await insertData(collection, extractedData);
+  } else {
+    console.log('No new data to insert.');
   }
+} catch (error) {
+  console.error('Error scraping data:', error);
+  throw error;
 }
+};
 
 module.exports = { loadGreenTradeData };
